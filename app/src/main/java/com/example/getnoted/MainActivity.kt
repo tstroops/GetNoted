eeacb66b2aaf53a07baf0cc63be53a09d151f1e2
tstroops <tomstroops@gmail.com>
2026-01-23 18:59:18 -0700

    I finally got the buttons on (onClick function is filler content)

--- app/src/main/java/com/example/getnoted/MainActivity.kt
+++ app/src/main/java/com/example/getnoted/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.getnoted.ui.theme.GetNotedTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GetNotedTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting(modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center))
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    var result = 1
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = {result=(1..6).random()}) {
+            Text(text ="Sign In")
+        }
+        Spacer(modifier= Modifier.height(16.dp))
+        Button(onClick = {result=(1..6).random()}) {
+            Text(text ="Sign Up")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    GetNotedTheme {
-        Greeting("Android")
+    Greeting()
     }
-}
