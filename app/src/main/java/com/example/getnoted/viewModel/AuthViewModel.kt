248a3ffb2d23dc8971f9a8f24b120acc62cb9477
Nick Sommer <som22006@byui.edu>
2026-02-18 14:13:36 -0700

    It just works. Got the Auth Section Completed

--- app/src/main/java/com/example/getnoted/viewModel/AuthViewModel.kt
+++ app/src/main/java/com/example/getnoted/viewModel/AuthViewModel.kt
@@ -1,8 +1,5 @@
 package com.example.getnoted.viewmodel
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.getnoted.data.AuthRepository
