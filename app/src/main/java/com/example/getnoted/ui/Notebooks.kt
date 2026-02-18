ec9affb099cd0eaefd3becce001caea310e4576e
tstroops <tomstroops@gmail.com>
2026-02-18 14:30:10 -0700

    added a notebook page

--- app/src/main/java/com/example/getnoted/ui/Notebooks.kt
+++ app/src/main/java/com/example/getnoted/ui/Notebooks.kt
@@ -1,2 +1,25 @@
 package com.example.getnoted.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun notebook(text:String){
+    val notebookNum = 3
+    Column {
+        for (i in 1..notebookNum){
+            Button(onClick = {/*TODO*/}) {
+                Text(text = "$text $i")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNotebook(){
+    notebook("Notebook")
+}
