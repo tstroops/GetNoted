945e0f9e3e0597b319283973e43c90cc5a0e754e
tstroops <tomstroops@gmail.com>
2026-03-26 16:08:56 -0600

    cleaned junk colors

--- app/src/main/java/com/example/getnoted/ui/theme/Color.kt
+++ app/src/main/java/com/example/getnoted/ui/theme/Color.kt
@@ -4,21 +4,9 @@ import androidx.compose.ui.graphics.Color
 
 // Brand Colors
 val BluePrimary = Color(0xFF2196F3)
-val BlueDark = Color(0xFF1976D2)
-val BlueLight = Color(0xFFBBDEFB)
 
 // Neutrals
 val White = Color(0xFFFFFFFF)
 val OffWhite = Color(0xFFFAFAFA)
 val Gray700 = Color(0xFF616161)
-val Gray200 = Color(0xFFEEEEEE)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val Black = Color(color = 0xFF000000)
